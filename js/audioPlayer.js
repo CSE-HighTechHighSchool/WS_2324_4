5f65f17379eabc6615a8fa2ecc5b023806713306
dewang1 <derek.zmw@gmail.com>
2024-01-09 23:30:12 -0500

    Sounds paywall
    Added themes paywall to sounds and commented out populate button

--- js/audioPlayer.js
+++ js/audioPlayer.js
@@ -1,4 +1,158 @@
+// ----------------- Firebase Setup & Initialization ------------------------//
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, signOut }
+  from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getDatabase, ref, set, update, child, get, remove }
+  from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBaZh0qXFMTkaKvX_08-WtJW4luzUFcirM",
+    authDomain: "gpa-saver-squared.firebaseapp.com",
+    databaseURL: "https://gpa-saver-squared-default-rtdb.firebaseio.com",
+    projectId: "gpa-saver-squared",
+    storageBucket: "gpa-saver-squared.appspot.com",
+    messagingSenderId: "1098242379811",
+    appId: "1:1098242379811:web:97a23ed43e6ce0ab5c2196"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication
+const auth = getAuth();
+
+// Returns instance of your apps' FRD
+const db = getDatabase(app);
+
+let currentUser = null; // Initialize currentUser to null
+// Custom alert function
+function customAlert(message) {
+  // Create overlay
+  let overlay = document.createElement('div');
+  overlay.className = 'overlay';
+
+  // Create popup
+  let popup = document.createElement('div');
+  popup.className = 'popup';
+
+  // Add message to popup
+  popup.innerText = message;
+
+  // Create close button
+  let closeButton = document.createElement('button');
+  closeButton.className = 'closeButton btn btn-dark rounded-pill';
+  closeButton.textContent = 'x';
+
+  // Add event listener to close button and overlay
+  closeButton.addEventListener('click', function() {
+    document.body.removeChild(overlay);
+  });
+
+  overlay.addEventListener('click', function() {
+    document.body.removeChild(overlay);
+  });
+
+  // Prevent event propagation to overlay when popup is clicked
+  popup.addEventListener('click', function(event) {
+    event.stopPropagation();
+  });
+
+  // Add close button to popup
+  popup.appendChild(closeButton);
+
+  // Add popup to overlay
+  overlay.appendChild(popup);
+
+  // Add overlay to body
+  document.body.appendChild(overlay);
+}
+//----------------- Dynamic navbar in tool page ------------------------//
+// Make navigation bar solid color if page is scrolled
+const navbar = document.getElementById("tools-navbar");
+
+if (navbar) {
+  document.addEventListener("scroll", (event) => {
+    if (window.scrollY > 0) {
+      navbar.style.backgroundColor = "var(--bg-nav)";
+    } else {
+      navbar.style.backgroundColor = "transparent";
+    }
+  });
+}
+
+//----------------------- Get User Object ------------------------------//
+function getUser() {
+    // Grab value for the 'keep logged in' switch
+    let keepLoggedIn = localStorage.getItem('keepLoggedIn');
+  
+    // Grab user information passed from signIn.js
+    if (keepLoggedIn == "yes") {
+      currentUser = JSON.parse(localStorage.getItem("user"));
+    } else {
+      currentUser = JSON.parse(sessionStorage.getItem("user"));
+    }
+}
+
+// ----------------------------- Get plan --------------------------
+// Return the user's plan ("basic" or "pro")
+async function getPlan(userID) {
+    let plan = null;
+    await get(ref(db, `users/${userID}/accountInfo`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          plan = snapshot.val()["plan"];
+        } else {
+          customAlert("No data found.");
+        }
+      })
+      .catch((error) => {
+        customAlert("Unsuccessful, error: " + error);
+      });
+    return plan;
+}
+
+// Sign-out function that will remove user info from local/session storage
+function signOutUser() {
+    sessionStorage.removeItem("user");  // Clear session storage
+    localStorage.removeItem("user");    // Clear local storage
+    localStorage.removeItem("keepLoggedIn");
+  
+    signOut(auth).then(() => {
+      // Sign-out successful
+    })
+    .catch((error) => {
+      customAlert("Error: " + error)
+    })
+  
+    window.location = "login.html"
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+  // ------------------------- Set Welcome Message -------------------------
+  const loginLink = document.getElementById("login-link");
+  const signupLink = document.getElementById("signup-link");
+
+  getUser();
+  if (currentUser != null) {
+      loginLink.innerHTML = '';
+      const userMessage = document.createElement("div");
+      userMessage.className = "nav-link nbMenuItem";
+      userMessage.style.color = "white";
+      userMessage.textContent = `Hello, ${currentUser.firstname}!`;
+      loginLink.appendChild(userMessage);
+
+      signupLink.innerHTML = '';
+      const logoutButton = document.createElement("button");
+      logoutButton.className = "btn btn-light nav-btn rounded-pill py-1";
+      logoutButton.textContent = "Log out";
+      logoutButton.onclick = function() {
+          signOutUser();
+      }
+      signupLink.appendChild(logoutButton);
+
+  }
   // Theme background
   var themes = [
     {
@@ -68,6 +222,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Add more themes as needed
   ];
 
+  
   // Target all buttons we need
   var audioPlayer = document.getElementById("audioPlayer");       // <audio> element
   var playPauseBtn = document.getElementById("playPauseBtn");     // Play and pause button
@@ -195,10 +350,25 @@ var fadeAlpha = 0;
   });
 
   // When theme button is clicked, apply the theme
-  changeThemeBtn.addEventListener("click", function () {
-    // Go on to the next theme and
-    // wrap back to the first theme if there are no more themes to go through
-    currentThemeIndex = (currentThemeIndex + 1) % themes.length;
+  changeThemeBtn.addEventListener("click", async function () {
+    // Increment currentThemeIndex
+    currentThemeIndex++;
+    getUser();
+    // Check if themes should loop
+    if(currentUser!=null){
+      let plan = await getPlan(currentUser.uid);
+      if (plan === "basic" && currentThemeIndex >= 3) {
+        currentThemeIndex = 0; // Reset to first theme
+        customAlert("Upgrade to Pro Plan to access all themes");
+      } else if (plan === "pro" && currentThemeIndex >= themes.length) {
+        // For pro users, loop all themes
+        currentThemeIndex = 0;
+      }
+    }
+    else{
+      currentThemeIndex = 0; // Reset to first theme
+      customAlert("Create a free account to access more themes");
+    }
     applyTheme(); // Apply the theme
   });
 
